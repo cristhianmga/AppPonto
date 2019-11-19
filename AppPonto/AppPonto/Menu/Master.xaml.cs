078ca7dd1ff6e106d5deae74408c1df9d78bb942
Cristhian de Almeida <cristhian.almeida@certsys.com.br>
2019-11-19 18:38:03 -0300

    Adicionar arquivos de projeto.

---
+++ AppPonto/AppPonto/Menu/Master.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPonto.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToActivityIndicator(object sender, EventArgs args)
+        {
+            Detail = new Controles.ActivityIndicatorPage();
+        }
+
+        private void GoToProgressBar(object sender, EventArgs args)
+        {
+            Detail = new Controles.ProgressBarPage();
+        }
+    }
+}
